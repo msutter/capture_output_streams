08e4ae47d640b1a06607005e7bd3586186d87978
Marc Sutter <marc.sutter@swisscom.com>
2014-11-13 14:27:24 +0100

    add backticks override

---
+++ lib/capture_output_streams/backticks.rb
@@ -0,0 +1,36 @@
+module CaptureOutputStream
+
+  Kernel.module_eval do
+
+    def `(*args)
+      command = args.join(' ')
+      status = Open4::popen4(command) do |pid, stdin, stdout, stderr|
+        @pid=pid
+        @stdin=command
+        @stdout=""
+        @stderr=""
+
+        while(line=stdout.gets)
+          @stdout+=line
+        end
+
+        while(line=stderr.gets)
+          @stderr+=line
+        end
+
+        unless @stdout.nil?
+          @stdout=@stdout.strip
+        end
+
+        unless @stderr.nil?
+          @stderr=@stderr.strip
+        end
+
+      end
+      @status = status.to_i
+      $stderr.puts @stderr unless @stderr.empty?
+      @stdout
+    end
+
+  end
+end
