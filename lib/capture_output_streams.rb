d37f6c6641d848c81d9488affeaed80984d767d2
Marc Sutter <marc.sutter@swisscom.com>
2014-11-13 18:36:00 +0100

    add dynamic kernel methods faking and restore

--- lib/capture_output_streams.rb
+++ lib/capture_output_streams.rb
@@ -1,27 +1,51 @@
 require 'open4'
 
-require_relative 'capture_output_streams/system'
-require_relative 'capture_output_streams/backticks'
-
 module CaptureOutputStreams
 
   Kernel.module_eval do
 
     def capture_output_streams(&block)
+      path = File.dirname(__FILE__)
+
+      # save original kernel methods
+      Kernel.module_eval do
+        alias_method :orig_system,    :system
+        alias_method :orig_backticks, :`
+      end
+
+      # save original output streams
       original_stdout = $stdout
       original_stderr = $stderr
+
+      # override original kernel methods
+      load File.join(path, 'capture_output_streams/system.rb')
+      load File.join(path, 'capture_output_streams/backticks.rb')
+
+      # override original output streams
       $stdout = fake_stdout = StringIO.new
       $stderr = fake_stderr = StringIO.new
+
       begin
         yield
+
       ensure
+        # restore original kernel methods
+        Kernel.module_eval do
+          alias_method :system, :orig_system
+          alias_method :`,      :orig_backticks
+        end
+
+        # restore original output streams
         $stdout = original_stdout
         $stderr = original_stderr
+
       end
+
       {
         :stdout => fake_stdout.string,
         :stderr => fake_stderr.string,
       }
+
     end
 
   end
